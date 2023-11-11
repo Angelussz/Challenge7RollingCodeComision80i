2d7e1ed8d10148a912347340714102c58457a05a
angelo <aperezr@unsa.edu.pe>
2023-11-11 00:14:00 -0500

    areaCirculo

---
+++ js/areaCirculo.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let radio = parseInt(prompt("Radio del circulo"));
+
+let pi = 3.14 //Se puede usar Math.PI? y Math.pow?
+let areaCirculo = pi*radio*radio;
+console.log(areaCirculo);
