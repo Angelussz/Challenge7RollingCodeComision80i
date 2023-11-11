6f55fc61cd21b9badf73586533e1a64dd4dac233
angelo <aperezr@unsa.edu.pe>
2023-11-11 00:07:12 -0500

    areaTriangulo

---
+++ js/areaTriangulo.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let base = parseInt(prompt("Base del triangulo"));
+let altura = parseInt(prompt("Altura del triangulo"));
+
+let areaTriangulo = (base*altura)/2;
+console.log(areaTriangulo);
